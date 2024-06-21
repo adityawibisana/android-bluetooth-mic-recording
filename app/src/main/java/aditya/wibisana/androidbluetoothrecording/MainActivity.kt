2605c4f41537411241b137fe1369f3b4077d8f74
Aditya Wibisana <aditya.wibisana@gmail.com>
2024-06-21 15:40:36 +0700

    Show connection status

--- app/src/main/java/aditya/wibisana/androidbluetoothrecording/MainActivity.kt
+++ app/src/main/java/aditya/wibisana/androidbluetoothrecording/MainActivity.kt
@@ -1,5 +1,9 @@
 package aditya.wibisana.androidbluetoothrecording
 
+import aditya.wibisana.androidbluetoothrecording.ui.theme.AndroidBluetoothRecordingTheme
+import aditya.wibisana.bluetooth.BluetoothConnectionDeviceListener
+import aditya.wibisana.bluetooth.BluetoothPermission
+import android.bluetooth.BluetoothAdapter
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,19 +13,28 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import aditya.wibisana.androidbluetoothrecording.ui.theme.AndroidBluetoothRecordingTheme
+import kotlinx.coroutines.flow.StateFlow
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val bluetoothPermission = BluetoothPermission()
+        bluetoothPermission.updateState(this.applicationContext)
+        val bluetoothListener = BluetoothConnectionDeviceListener(
+            context = applicationContext,
+            bluetoothAdapter = BluetoothAdapter.getDefaultAdapter(),
+            bluetoothPermission = bluetoothPermission
+        )
+
         setContent {
             AndroidBluetoothRecordingTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    DeviceStatus(
+                        status = bluetoothListener.status,
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +44,13 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun DeviceStatus(
+    status: StateFlow<BluetoothConnectionDeviceListener.ConnectionStatus>,
+    modifier: Modifier = Modifier
+) {
+    val currentStatus = status.collectAsState()
     Text(
-        text = "Hello $name!",
+        text = "Device is: ${currentStatus.value.name}!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidBluetoothRecordingTheme {
-        Greeting("Android")
-    }
 }
